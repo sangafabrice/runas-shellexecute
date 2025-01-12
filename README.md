0760d962082684cf43b423f8a2fd1e964753e42b
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2025-01-12 21:46:38 +0100

    feat: Pause script execution in PowerShell

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## **Examples**
+
 ```powershell
 PS> wscript admin.js "powershell" "-NoExit"
 
@@ -6,4 +8,16 @@ PS> wscript admin.vbs "powershell" "-NoExit"
 PS> powershell -NoProfile -File admin.ps1 "powershell" "-NoExit"
 
 PS> pwsh -NoProfile admin.ps1 "powershell" "-NoExit"
+```
+
+## **Test Limited Access**
+
+It is preferable to start the next PowerShell with the argument `-NoProfile`.
+
+```powershell
+PS> Set-PSBreakpoint -Script admin.ps1 -Line 33 -Action {
+  Wait-Process -Id $childProcess.Id 2>&1 | Out-Host
+}
+
+PS> .\admin.ps1 "powershell" "-NoExit"
 ```
--- admin.ps1
+++ admin.ps1
@@ -1,5 +1,7 @@
 using namespace System.Runtime.InteropServices
 using namespace System.Reflection
+using namespace Shell32
+using assembly Interop.Shell32.dll
 
 function Start-AdminProcess {
   [CmdletBinding()]
@@ -7,12 +9,32 @@ function Start-AdminProcess {
     # The path to the program to execute.
     [string] $programExe,
     # The arguments string used to run the program.
-    [string] $arguments
+    [string] $arguments,
+    # Specifies that the script pauses til the process exits.
+    [switch] $wait
   )
-  $Shell32 = New-Object -ComObject 'Shell.Application' # Late-binding
+  $Shell32 = [ShellClass]::new()
   $Shell32.ShellExecute($programExe, $arguments, [Missing]::Value, 'runas')
   [void][Marshal]::FinalReleaseComObject($Shell32)
   $Shell32 = $null
+  if ($wait) {
+    Wait-ChildProcess
+  }
 }
 
+function Wait-ChildProcess {
+  [CmdletBinding()]
+  param ()
+  $childProcess =
+    Get-CimInstance Win32_Process -Filter "ParentProcessID='$PID'" |
+    Select-Object @{ Label = 'Id'; Expression = { $_.ProcessId } } |
+    Get-Process -ErrorAction SilentlyContinue
+  if ($null -ne $childProcess) {
+    $childProcess.WaitForExit()
+    $childProcess.Dispose()
+  }
+}
+
+Start-AdminProcess $args[0] $args[1] -wait
+Start-AdminProcess $args[0] $args[1]
 Start-AdminProcess $args[0] $args[1]
